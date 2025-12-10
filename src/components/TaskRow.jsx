0336e191d835be1586a159d8b8d5d74d27d9c530
Marco Bacci <marcobacci94@gmail.com>
2025-12-10 11:55:34 +0100

    Feat: change status background color

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,10 +1,21 @@
 const TaskRow = ({ task }) => {
+  const getStatusColor = (status) => {
+    switch (status) {
+      case "To do":
+        return { backgroundColor: "rgb(255, 120, 120)" };
+      case "Doing":
+        return { backgroundColor: "rgb(255, 230, 120)" };
+      case "Done":
+        return { backgroundColor: "rgb(140, 255, 160)" };
+      default:
+        return {};
+    }
+  };
+
   return (
     <tr>
       <td>{task.title}</td>
-      <td>
-        {task.status}
-      </td>
+      <td style={getStatusColor(task.status)}>{task.status}</td>
       <td>{new Date(task.createdAt).toLocaleDateString()}</td>
     </tr>
   );
