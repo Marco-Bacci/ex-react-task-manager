8e72fe2744c052232f28e3631bc10ea27105a586
Marco Bacci <marcobacci94@gmail.com>
2025-12-11 11:47:27 +0100

    Feat: add link to title. (milestone 7) step 1 completed

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,4 +1,5 @@
 import { memo } from "react";
+import { Link } from "react-router-dom";
 
 const TaskRow = memo(({ task }) => {
   const getStatusColor = (status) => {
@@ -16,7 +17,10 @@ const TaskRow = memo(({ task }) => {
 
   return (
     <tr>
-      <td>{task.title}</td>
+      <td>
+        <Link to={`/task/${task.id}`}>{task.title}</Link>
+      </td>
+
       <td style={getStatusColor(task.status)}>{task.status}</td>
       <td>{new Date(task.createdAt).toLocaleDateString()}</td>
     </tr>
