843a73e007cf46d91384e8d3deb178f27e0ca8e1
Marco Bacci <marcobacci94@gmail.com>
2025-12-11 12:29:02 +0100

    Feat: (milestone 8) step 1 completed

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -21,7 +21,15 @@ function useTasks() {
     if (!success) throw new error(message);
     setTasks((prev) => [...prev, task]);
   };
-  const removeTask = () => {};
+  const removeTask = async (taskId) => {
+    const response = await fetch(`${VITE_API_URL}/tasks/${taskId}`, {
+      method: "DELETE",
+    });
+    const { success, message } = await response.json();
+    if (!success) throw new error(message);
+    
+    setTasks((prev) => prev.filter((t) => t.id !== taskId));
+  };
   const updateTask = () => {};
 
   return { tasks, addTask, removeTask, updateTask };
