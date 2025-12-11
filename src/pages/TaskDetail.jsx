986779aa655a54886d56a074bbb2da9ddf24a614
Marco Bacci <marcobacci94@gmail.com>
2025-12-11 12:09:53 +0100

    Feat: add detailPage

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-
+import { useParams } from "react-router-dom";
+import { GlobalContext } from "../contexts/GlobalContext";
+import { useContext } from "react";
 const TaskDetail = () => {
-  return (
-    <div>
-      <h2>PAGINA DI DETTAGLIO</h2>
-    </div>
-  )
-}
+  const { id } = useParams();
+  const { tasks } = useContext(GlobalContext);
 
-export default TaskDetail
+  const task = tasks.find((t) => t.id === parseInt(id));
+
+  if (!task) {
+    return <h2>Task non trovata</h2>;
+  }
+  const handleDelete = () =>{
+    console.log("task eliminata:", task.id)
+  }
+
+  return (
+    <div className="container">
+      <h2>Dettaglio della Task</h2>
+      <p>
+        <strong>Nome Task:</strong> {task.title}{" "}
+      </p>
+      <p>
+        <strong>Descrizione:</strong> {task.description}
+      </p>
+      <p>
+        <strong>Stato:</strong> {task.status}
+      </p>
+      <p>
+        <strong>Data di creazione:</strong>
+        {new Date(task.createdAt).toLocaleDateString()}
+      </p>
+      <button onClick={handleDelete}> Elimina Task</button>
+    </div>
+  );
+};
+
+export default TaskDetail;
